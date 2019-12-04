e323cd3c54f923e279be483cc500e64b27bbfe60
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-04 15:01:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,4 +6,7 @@ doc.css(".headline-26OIBN").text
 
 p doc.css(".headline-26OIBN").text.strip
 
+html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
 doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
